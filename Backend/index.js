7d56370caa146a28d772b4f161a549fa04b15a9f
Hetvimodi6204 <139551223+Hetvimodi6204@users.noreply.github.com>
2024-12-25 22:43:06 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['https://soulful-saga-front2.vercel.app'],
+    origin: ['https://soulful-saga-front3.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, 
 }));
