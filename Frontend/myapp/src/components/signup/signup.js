c8a85d11a1819f733c38b6494989fd67040e8feb
Hetvimodi6204 <139551223+Hetvimodi6204@users.noreply.github.com>
2024-12-25 22:27:07 +0530

    Update signup.js

--- Frontend/myapp/src/components/signup/signup.js
+++ Frontend/myapp/src/components/signup/signup.js
@@ -27,7 +27,7 @@ const Signup = () => {
         const { name, email, password, reEnterPassword } = user;
 
         if (name && email && password && (password === reEnterPassword)) {
-            fetch("https://soulful-saga.vercel.app/signup", {
+            fetch("https://soulful-saga-back1.vercel.app/signup", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
